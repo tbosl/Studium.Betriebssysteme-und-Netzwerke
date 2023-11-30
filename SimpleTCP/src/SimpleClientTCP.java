d8febb9d1f50b2c200db3dcb464c4fb589c02878
Tobias Bosl <tboslffb@gmail.com>
2023-11-30 17:50:24 +0100

    add: Versuch

--- SimpleTCP/src/SimpleClientTCP.java
+++ SimpleTCP/src/SimpleClientTCP.java
@@ -6,6 +6,8 @@ import java.util.*;
 public class SimpleClientTCP {
     static int PORT = 1234;
     static Scanner inFromUser = null;
+    static long startTime;
+    static long endTime;
 
     public static void main(String args[]) throws Exception {
         System.out.printf("Client gestartet\n");
@@ -13,31 +15,31 @@ public class SimpleClientTCP {
         inFromUser = new Scanner(System.in);
         System.out.printf("Bitte IP-Adresse des Servers eingeben: ");
         String server = inFromUser.nextLine();
-        InetAddress address = InetAddress.getByName("192.168.178.24");
-
-        Socket socket = new Socket(address, PORT);
-
-        protocol(socket);
+        InetAddress address = InetAddress.getByName("127.0.0.1");
+        System.out.printf("Was wollen Sie dem Server senden? ");
+        String request = inFromUser.nextLine();
+        for (int i = 0; i < 10000; i++) {
+            Socket socket = new Socket(address, PORT);
+            if (i == 0) {
+                startTime = System.currentTimeMillis();
+            }
+            protocol(socket, i + ". " + request);
+        }
         inFromUser.close();
+        System.out.println("Dauer der Übertragung: " + (endTime - startTime));
     }
 
-    public static void protocol(Socket socket) throws Exception {
-
+    public static void protocol(Socket socket, String request) throws Exception {
         Scanner inFromServer = new Scanner(socket.getInputStream());
         PrintStream outToServer = new PrintStream(socket.getOutputStream());
 
-        System.out.printf("Was wollen Sie dem Server senden? ");
-        String request = inFromUser.nextLine();
-        long startTime = System.currentTimeMillis();
-        for (int currentRequest = 1; currentRequest < 10000; currentRequest++) {
-            outToServer.println(currentRequest + ". " + request);
+        outToServer.println(request);
 
-            //while (inFromServer.hasNextLine()) {
+        while (inFromServer.hasNextLine()) {
             String reply = inFromServer.nextLine();
             System.out.println("RECEIVED FROM SERVER: " + reply);
-            //}
         }
-        System.out.println("Dauer der Übertragung: " + (System.currentTimeMillis() - startTime));
+        endTime = System.currentTimeMillis();
         socket.close();
     }
 }
--- SimpleTCP/src/SimpleServerTCP.java
+++ SimpleTCP/src/SimpleServerTCP.java
@@ -3,30 +3,30 @@ import java.net.*;
 import java.util.*;
 
 public class SimpleServerTCP {
-        static int PORT = 1234;
+    static int PORT = 1234;
 
-	public static void main(String args[]) throws Exception {
-		System.out.printf("Server gestartet\n");
+    public static void main(String args[]) throws Exception {
+        System.out.printf("Server gestartet\n");
 
-                ServerSocket servsocket = new ServerSocket(PORT);
+        ServerSocket servsocket = new ServerSocket(PORT);
 
-                while (true) {
-                        Socket socket = servsocket.accept();
-                        protocol(socket);
-                }
-	}
+        while (true) {
+            Socket socket = servsocket.accept();
+            protocol(socket);
+        }
+    }
 
-	public static void protocol(Socket socket) throws Exception {
+    public static void protocol(Socket socket) throws Exception {
 
-                Scanner inFromClient = new Scanner(socket.getInputStream());
-                PrintStream outToClient = new PrintStream(socket.getOutputStream());
+        Scanner inFromClient = new Scanner(socket.getInputStream());
+        PrintStream outToClient = new PrintStream(socket.getOutputStream());
 
-                String request = inFromClient.nextLine();
-                System.out.println("RECEIVED FROM CLIENT: " + request);
+        String request = inFromClient.nextLine();
+        System.out.println("RECEIVED FROM CLIENT: " + request);
 
-                outToClient.println("I am the server. This is what I heard from you:");
-                outToClient.println(request);
+        outToClient.println("I am the server. This is what I heard from you:");
+        outToClient.println(new StringBuilder(request).reverse());
 
-                socket.close();
-	}
+        socket.close();
+    }
 }
